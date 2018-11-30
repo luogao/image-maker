825f5d630ea33cb7bfa079518f34110378c44e5f
luogao <luogao_lg@sina.com>
2018-11-30 16:42:32 +0800

    fix bug

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,6 @@
 const path = require('path')
-
-exports.DEFAULTOUTPUTBASENAME = 'im-output'
+const DEFAULTOUTPUTBASENAME = 'im-output'
+exports.DEFAULTOUTPUTBASENAME
 exports.CUSTOMCONFIGPATH = path.resolve(
   process.cwd(),
   path.join('imconfig.json')
--- utils/index.js
+++ utils/index.js
@@ -16,7 +16,6 @@ exports.fileNameGenerator = (output, originalName, type, size) => {
 }
 
 exports.makeDir = dir => {
-  console.log(dir)
   try {
     //是否能访问到这个文件，如果能访问到，说明这个文件已经存在，进入循环的下一步。
     //accessSync的第二个参数就是用来判断该文件是否能被读取
