6895a220ed7ce1cf17629d155c19fbccab8429e1
Roy <luogao_lg@sina.com>
2019-01-18 09:41:53 +0800

    format fixed

--- constants/index.js
+++ constants/index.js
@@ -2,10 +2,12 @@ const path = require('path')
 const DEFAULTOUTPUTBASENAME = 'im-output'
 
 exports.DEFAULTOUTPUTBASENAME
+
 exports.CUSTOMCONFIGPATH = path.resolve(
   process.cwd(),
   path.join('imconfig.json')
 )
+
 exports.DEFAULTOUTPUT = path.resolve(
   process.cwd(),
   path.join('.', DEFAULTOUTPUTBASENAME)
